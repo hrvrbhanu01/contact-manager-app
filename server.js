583e7d2c52bed4395d9dbe24dfb3eb25e0b60dcc
Bhanu Prakash Dixit <74755575+hrvrbhanu01@users.noreply.github.com>
2023-11-28 17:23:47 +0530

    routes

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const dotenv = require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
+
+app.use("/api/contacts", require("./routes/contactRoute"));
 
 app.listen(port, ()=> {
     console.log(`Server is running on ${port}`);
