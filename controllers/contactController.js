5b679a7ed0ff2ef265023b4b301d7888a50e7230
Bhanu Prakash Dixit <74755575+hrvrbhanu01@users.noreply.github.com>
2023-11-28 18:12:01 +0530

    contollers

---
+++ controllers/contactController.js
@@ -0,0 +1,47 @@
+//for db connection
+
+//@description for Get all contacts
+//@route GET /api/contacts
+//@access to the api:  public
+
+const getContacts = (req, res) => {
+    res.status(200).json({ message: "Get all contacts"})
+}
+
+
+//@description for Create new contact
+//@route POST /api/contacts
+//@access to the api:  public
+
+const createContact = (req, res) => {
+    res.status(201).json({ message: "Create contact"})
+}
+
+
+//@description for Get contact
+//@route GET /api/contacts
+//@access to the api:  public
+
+const getContact = (req, res) => {
+    res.status(200).json({ message: `Get contact for ${req.params.id}`})
+}
+
+//@description for Update contact
+//@route PUT /api/contacts
+//@access to the api:  public
+
+const updateContact = (req, res) => {
+    res.status(200).json({ message: `Update contact for ${req.params.id}`})
+}
+
+
+//@description for Delete contact
+//@route DELETE /api/contacts
+//@access to the api:  public
+
+const deleteContact = (req, res) => {
+    res.status(200).json({ message: `Delete contact for ${req.params.id}`})
+}
+
+
+module.exports = { getContacts, createContact, getContact, updateContact, deleteContact }  //for exposing out
--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -2,24 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-router.route("/").get((req, res) => {
-    req.status(200).json({ message: "Get all contacts"});
-});
+const { 
+    getContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact,
+} = require("../controllers/contactController")
 
-router.route("/").post((req, res) => {
-    req.status(200).json({ message: "Create contact"});
-});
+// router.route("/retrieveSecrets/:secretName").get(retrieveSecrets);
+// router.post("/storeSecrets", storeSecrets)
+router.route("/").get(getContacts).post(createContact);
 
-router.route("/:id").get((req, res) => {
-    req.status(200).json({ message: `Get contact for ${req.params.id}`});
-});
-
-router.route("/:id").put((req, res) => {
-    req.status(200).json({ message: `Update contact for ${req.params.id}`});
-});
-
-router.route("/:id").delete((req, res) => {
-    req.status(200).json({ message: `Delete contact for ${req.params.id}`});
-});
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact)
 
 module.exports = router;
