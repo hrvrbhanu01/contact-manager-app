c3961a49ef15db79205d6149fe67e2ec548d5e66
Bhanu Prakash Dixit <74755575+hrvrbhanu01@users.noreply.github.com>
2023-12-04 14:36:26 +0530

    cC

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -6,7 +6,7 @@
 //in case of async , if you want to catch an error you need to use try and catch block in that case.
 
 
-const asyncHandleer = require("express-async-handler")
+const asyncHandler = require("express-async-handler")
 //@description for Get all contacts
 //@route GET /api/contacts
 //@access to the api:  public
@@ -20,7 +20,7 @@ const getContacts = asyncHandler(async (req, res) => {
 //@route POST /api/contacts
 //@access to the api:  public
 
-const createContact = async (req, res) => {
+const createContact = asyncHandler( async (req, res) => {
     console.log("The request body is: ", req.body);
     const {name, email, phone}=req.body;
     if(!name || !email || !phone){
@@ -28,33 +28,33 @@ const createContact = async (req, res) => {
         throw new Error("All fields are mandatory !");
     };
     res.status(201).json({ message: "Create contact"})
-}
+})
 
 
 //@description for Get contact
 //@route GET /api/contacts
 //@access to the api:  public
 
-const getContact = async (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Get contact for ${req.params.id}`})
-}
+})
 
 //@description for Update contact
 //@route PUT /api/contacts
 //@access to the api:  public
 
-const updateContact = async (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update contact for ${req.params.id}`})
-}
+})
 
 
 //@description for Delete contact
 //@route DELETE /api/contacts
 //@access to the api:  public
 
-const deleteContact = async (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete contact for ${req.params.id}`})
-}
+})
 
 
 module.exports = { getContacts, createContact, getContact, updateContact, deleteContact }  //for exposing out
