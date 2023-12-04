2fcf1b27f394c736b16f1538eb6339f685a987e8
Bhanu Prakash Dixit <74755575+hrvrbhanu01@users.noreply.github.com>
2023-12-04 14:48:06 +0530

    .

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const dotenv = require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoute"));
 app.use(errorHandler);
 
 app.listen(port, ()=> {
-    console.log(`Server is running on ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
