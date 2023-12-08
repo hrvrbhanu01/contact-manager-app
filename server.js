01b79bb53efa6932d92f89d11cf2fc67da575a78
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2023-12-08 17:41:31 +0530

    authentication routes

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoute"));
+app.use("/api/users", require("./routes/userRoutes"));
+
 app.use(errorHandler);
 
 app.listen(port, ()=> {
