99992a945d9a0a1bc3b4e71034590ba47e3852f1
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2023-12-10 17:10:15 +0530

    user creation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,40 @@
 const asyncHandler= require("express-async-handler")
-
+const bcrypt=require("bcrypt")
 //@description for register a user
 //@route POST /api/users/register
 //@access to the api:  public
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Register the user" })
+    const { username, email, password } = req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All the fields are mandatory...")
+    }
+
+    const userAvailable = await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already exists...")
+    }
+
+    //hash Password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed Password", hashedPassword)
+
+    const user=await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    })
+
+    console.log(`User Created ${user}`)
+    if(user){
+        res.status(201).json({ _id: user.id, email: user.email })
+    }else{
+        res.status(400);
+        throw new Error("User data is not valid!");
+    }
+    res.json({ message: "Register the user" });
 })
 
 //@description for login a user
